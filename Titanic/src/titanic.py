0bea34939da7eebd0b01dbf76a3b899477d648de
AkihiroOkuda <ScuderiaFerrariSebastian@gmail.com>
2022-09-08 22:00:03 +0900

    データの読み込みまで実施

--- Titanic/src/titanic.py
+++ Titanic/src/titanic.py
@@ -2,3 +2,6 @@ import numpy as np
 import pandas as pd 
 
 
+train = pd.read_csv('../data/train.csv')
+test = pd.read_csv('../data/test.csv')
+gender_submission = pd.read_csv('../data/gender_submission.csv')
